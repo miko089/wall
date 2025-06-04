b73d4b546f7f3bc87c6ddf44335560d37d886856
just-mn <77879381+just-mn@users.noreply.github.com>
2025-06-05 05:07:30 +0600

    fix date timezone formating

--- public/app.js
+++ public/app.js
@@ -16,11 +16,14 @@
         .map(([k,v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
         .join("&");
 
-    const fmtDate = iso =>
-        new Date(iso*1000).toLocaleString("ru-RU", {
+    const fmtDate = iso => {
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        return new Date(iso * 1000).toLocaleString("ru-RU", {
+            timeZone: userTimeZone,
             year: "numeric", month: "2-digit", day: "2-digit",
             hour: "2-digit", minute: "2-digit", second: "2-digit",
         });
+    };
 
     const escapeHtml = str => str
         .replace(/&/g, "&amp;")
